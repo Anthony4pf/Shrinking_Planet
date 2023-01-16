87528e138105348ccc7f81ee496c0362c02efb0a
Anthony4pf <edeanthony14@gmail.com>
2023-01-15 23:35:35 -0800

    Add Project Files

---
+++ Shrinking Planet/Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FPSDisplay : MonoBehaviour
+{
+    public int avgFrameRate;
+    public Text display_Text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+    }
+}
